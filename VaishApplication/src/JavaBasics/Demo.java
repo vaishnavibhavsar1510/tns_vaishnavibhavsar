817603ed7550a3acf60cd570f05a4e2937c70552
vaishnavibhavsar1510 <vaishnavibhavsar03@gmail.com>
2024-06-28 12:03:41 +0530

    commit

---
+++ VaishApplication/src/JavaBasics/Demo.java
@@ -0,0 +1,97 @@
+
+package JavaBasics;
+   
+public class Demo {
+	public static void divide(int arr[], int low , int high ) {
+		
+	}
+	
+	
+	public static void main(String args[]) {
+		//Primitive datatype - fixed memory spaces, declared by Java only, 
+//		  /              \
+// Numeric (4 Types)     Non-Numeric    //1 byte = 8 bits
+//    /           \          /      \
+//  Integer   Floating pt.  Bool[1]   Char[16 bits]
+//  (byte[8]   float[32]
+//  short[16]  double[64]
+//  int[32]
+//  long[64])
+//  
+		
+  
+	                
+	  // Non- Primitive - Non predefined memory space , data types are declared by java 
+//	     /	    |    \
+//	 string  array  etc 	
+		
+		
+	   
+		
+//		
+//		int a = 2;
+//		System.out.println(a);
+//		int var = ++a;
+//		System.out.println(var);
+//		int var1 = a++;
+//		System.out.println(var1);
+////		
+////		
+//		
+//	int c = 44;
+//	int	a = 11; 
+//	int b = 20 ;
+//	
+//	
+//	 //    44
+//	int d =c++ + a + b--;
+//	System.out.println(d);
+
+//		int var = 9/2 ; 
+//		System.out.print(var);
+//		
+//		long var1 = 999999998800000l;
+//		
+//		Syastem.out.println("")
+//		sys.out 
+		
+		
+		//Type casting -- narrowing and widening 
+	
+	
+	
+	
+	
+	
+	
+  	
+	
+	
+	  //Looping and branching 
+//	  
+//	  System.out.println("Pa mss the input marks for grades ");
+//	  Scanner marks = new Scanner(System.in) ;
+//	  int input = marks.nextInt();
+//	  System.out.println("Enter marks :" + input);
+//	  
+//	  
+//	  if (marks < 35 ) {
+//		  System.out.println(h)
+//	  }
+      
+	   for (int i = 1; i<=5; i++) {
+		   System.out.println("*");
+		   
+	   }
+	
+	
+	
+	
+	
+		
+	}
+
+	
+	
+
+}
---
+++ VaishApplication/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module VaishApplication {
+}
